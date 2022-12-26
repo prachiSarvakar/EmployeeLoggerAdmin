705850d77a3b6943fbdc9fbe61030cbc50c3749f
prachi <prachi.sarvankar@neosoftmail.com>
2022-12-26 20:16:04 +0530

    eslint error resolved

--- src/Pages/EmployeeDataTable.jsx
+++ src/Pages/EmployeeDataTable.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React from "react";
 import { useSearchParams, useLocation } from "react-router-dom";
 import { Divider } from "@mui/material";
